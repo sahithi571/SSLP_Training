115598dc902188a3aa8fb2326ca575d3e4255a7b
kaushiknatua <kaushiknatua@yahoo.in>
2020-10-09 14:57:50 +0530

    Collections

---
+++ Collections/Class_Collection/Class_Collection/EmployeeBO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Collection
+{
+    public class EmployeeBO
+    {
+        public void DisplayAllEmployees(List<Employee> empList)
+        {
+            Console.WriteLine("{0,-20}{1,-20}{2}", "Employee Id","Employee Name", "Joining Date");
+            foreach(Employee employee in empList)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
---
+++ Collections/Class_Collection/Class_Collection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = new List<Employee>();
+            string loopInput = string.Empty;
+            do
+            {
+                Console.WriteLine("Enter Employee Id:");
+                int empId = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Employee Name:");
+                string empName = Console.ReadLine();
+                Console.WriteLine("Enter Joining Date(MM/dd/yyyy):");
+                DateTime empDOJ = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy", null);
+
+                Employee emp = new Employee(empId, empName, empDOJ);
+                employeeList.Add(emp);
+                Console.WriteLine("Press yes to continue..Any other key to terminate:");
+                loopInput = Console.ReadLine();
+            }
+            while (loopInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase));
+
+            Console.WriteLine("\n*********************************");
+            EmployeeBO bo = new EmployeeBO();
+            bo.DisplayAllEmployees(employeeList);
+        }
+    }
+}
